52685789b4f49bafe3c64ae973d61cb501ebdbd0
KrishaParmar <krisha.parmar@fxis.ai>
2024-12-17 11:33:36 +0530

    learning props

---
+++ 3_props_with_functional_and_class_components/src/Studentc.js
@@ -0,0 +1,12 @@
+import React from "react";
+export class Studentc extends React.Component{
+    render(){
+        console.log(this.props);
+        
+        return(
+            <div>
+                <h1>{this.props.name}</h1>
+            </div>
+        )
+    }
+}
