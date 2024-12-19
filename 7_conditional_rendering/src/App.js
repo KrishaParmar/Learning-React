3604ba5ad847baa345556d9760c499cf47de4bf9
KrishaParmar <krisha.parmar@fxis.ai>
2024-12-19 17:38:30 +0530

    conditinal_rendering

---
+++ 7_conditional_rendering/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Pro from "./profile"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Pro/>
+    </div>
+  );
+}
+
+export default App;
