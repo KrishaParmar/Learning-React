fb1cdd0d96df4984b2abecb9bb2c15a529fdb2f4
KrishaParmar <krisha.parmar@fxis.ai>
2024-12-26 07:29:34 +0530

    useEffect

--- 16_hooks/src/App.js
+++ 16_hooks/src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState,useEffect, use} from 'react';
+
+//-----------------useState-------------
+
+// function App() {
+//   const [data,setData] = useState("krisha")
+//   return (
+//     <div className="App">
+//       <h1>useState</h1>
+//       <h2>{data}</h2>
+//       <button onClick={()=>(setData("parmar"))}>update name</button>
+//     </div>
+//   );
+// }
+
+//----------------------------useEffect-----------
 
 function App() {
-  const [data,setData] = useState("krisha")
+  const [count,setcount] = useState(0)
+  useEffect(()=>{
+    console.log("hello")
+  })
   return (
     <div className="App">
-      <h1>useState</h1>
-      <h2>{data}</h2>
-      <button onClick={()=>(setData("parmar"))}>update name</button>
+      <h1>useEffect</h1>
+      <h2>{count}</h2>
+      <button onClick={()=>setcount(count+1)}>+ count</button>
     </div>
   );
 }
