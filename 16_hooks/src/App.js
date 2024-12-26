806cca8968b259fb4b9a7ab981ce79149472105f
KrishaParmar <krisha.parmar@fxis.ai>
2024-12-26 07:58:28 +0530

    useEffect with conditional props

--- 16_hooks/src/App.js
+++ 16_hooks/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User'
 import React,{useState,useEffect} from 'react';
 
 //-----------------useState-------------
@@ -33,21 +34,39 @@ import React,{useState,useEffect} from 'react';
 
 //----------------useEffect with conditional state------------------
 
+// function App() {
+//   const [count,setcount] = useState(100)
+//   const [data,setdata] = useState(10)
+
+//   useEffect(()=>{
+//     console.log("update count")
+//   },[count])
+//   useEffect(()=>{
+//     console.log("update data")
+//   },[data])
+//   return (
+//     <div className="App">
+//       <h1>useEffect with conditional state</h1>
+//       <h2>{count}</h2>
+//       <h2>{data}</h2>
+
+//       <button onClick={()=>setcount(count+1)}>+ count</button>
+//       <button onClick={()=>setdata(data+1)}>+ data</button>
+
+//     </div>
+//   );
+// }
+
+//--------------------useEffect with conditional props--------------
 function App() {
   const [count,setcount] = useState(100)
   const [data,setdata] = useState(10)
 
-  useEffect(()=>{
-    console.log("update count")
-  },[count])
-  useEffect(()=>{
-    console.log("update data")
-  },[data])
+ 
   return (
     <div className="App">
-      <h1>useEffect with conditional state</h1>
-      <h2>{count}</h2>
-      <h2>{data}</h2>
+      <h1>useEffect with conditional props</h1>
+      <User count={count} data={data}/>
 
       <button onClick={()=>setcount(count+1)}>+ count</button>
       <button onClick={()=>setdata(data+1)}>+ data</button>
